b71215ce3ba343c2e66602b71f0d8692d745548f
Marko Kovač <124689071+markokovac16@users.noreply.github.com>
2023-11-13 19:19:19 +0100

    Using real ticket data from the backend instead of using a placeholder

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,34 +1,34 @@
-"use client";
-
 import DeleteBlock from "./DeleteBlock";
 import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
-import React, { useState } from "react";
 
-const TicketCard = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
-
+const TicketCard = ({ ticket }) => {
+  const createdAt = new Date(ticket.createdAt);
+  const formattedDate = createdAt.toLocaleString();
+  const updatedAt = new Date(ticket.updatedAt);
+  const updatedDate = updatedAt.toLocaleString();
   return (
-    <div className="flex flex-col p-3 m-2 rounded-md shadow-lg bg-card hover:bg-card-hover">
+    <div className="flex flex-col p-3 m-2 transition duration-500 ease-in-out rounded-md shadow-lg bg-card hover:bg-card-hover hover:cursor-pointer">
       <div className="flex mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
 
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px mb-2 border-0 bg-page" />
-      <p className="whitespace-pre-wrap">Ticket description</p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
-        <div className="flex flex-col">
-          <p>Date of ticket: {currentDate.toLocaleDateString()}</p>
-          <ProgressDisplay />
+        <div className="flex flex-col gap-1">
+          <p>Created at: {formattedDate}</p>{" "}
+          {updatedDate !== formattedDate && <p>Updated at: {updatedDate}</p>}
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="flex items-end ml-auto">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status} />
         </div>
       </div>
     </div>
