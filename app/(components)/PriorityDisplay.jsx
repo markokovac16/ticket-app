a907d51d3e4c04c82d45a3d68a2be3a87324ef62
Marko Kovač <124689071+markokovac16@users.noreply.github.com>
2023-11-18 16:21:57 +0100

    refactor: more concise code for displaying priority level

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -2,28 +2,18 @@ import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PriorityDisplay = ({ priority }) => {
+  const maxPriority = 5;
   return (
     <div className="flex justify-start align-baseline">
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 0 ? "text-red-600" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 1 ? "text-red-600" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 2 ? "text-red-600" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 3 ? "text-red-600" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 4 ? "text-red-600" : "text-slate-400"}`}
-      />
+      {Array.from({ length: maxPriority }).map((_, index) => (
+        <FontAwesomeIcon
+          key={index}
+          icon={faFire}
+          className={`pr-1 ${
+            priority > index ? "text-red-600" : "text-slate-400"
+          }`}
+        />
+      ))}
     </div>
   );
 };
